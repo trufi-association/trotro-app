2e3892f646bf338c899fa767705d22fb37b196f5
Raimund Wege <raimundwege@gmail.com>
2019-10-24 14:54:07 +0200

    Updated translations

--- lib/translations/messages_all.dart
+++ lib/translations/messages_all.dart
@@ -9,17 +9,37 @@ import 'package:intl/message_lookup_by_library.dart';
 // ignore: implementation_imports
 import 'package:intl/src/intl_helpers.dart';
 
+import 'messages_de_DE.dart' deferred as messages_de_de;
 import 'messages_en_US.dart' deferred as messages_en_us;
+import 'messages_es_ES.dart' deferred as messages_es_es;
+import 'messages_fr_FR.dart' deferred as messages_fr_fr;
+import 'messages_it_IT.dart' deferred as messages_it_it;
+import 'messages_qu_BO.dart' deferred as messages_qu_bo;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
+  'de_DE': messages_de_de.loadLibrary,
   'en_US': messages_en_us.loadLibrary,
+  'es_ES': messages_es_es.loadLibrary,
+  'fr_FR': messages_fr_fr.loadLibrary,
+  'it_IT': messages_it_it.loadLibrary,
+  'qu_BO': messages_qu_bo.loadLibrary,
 };
 
 MessageLookupByLibrary _findExact(localeName) {
   switch (localeName) {
+    case 'de_DE':
+      return messages_de_de.messages;
     case 'en_US':
       return messages_en_us.messages;
+    case 'es_ES':
+      return messages_es_es.messages;
+    case 'fr_FR':
+      return messages_fr_fr.messages;
+    case 'it_IT':
+      return messages_it_it.messages;
+    case 'qu_BO':
+      return messages_qu_bo.messages;
     default:
       return null;
   }
